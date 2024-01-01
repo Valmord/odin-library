df5d2aa732acc0856019fb3b9d806cf3bffe258f
TC <nothing@nothing.com>
2024-01-02 03:24:34 +1300

    Modal window created for Add Book

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
+const libraryOptions = {
+    tableShown: false,
+}
+
 const myLibrary = [
     {   title:"The Secret of Terror Castle", 
         author:"Robert Arthur", 
@@ -33,11 +37,28 @@ function addBookToLibrary(title, author, pages, haveRead){
     myLibrary.push(newBook);
 }
 
-const displayBooksBtn = document.querySelector('.display-books-btn');
-displayBooksBtn.onclick = displayLibrary;
-const booksTable = document.querySelector('.books-table');
+function updateLibraryInfo(){
+    libraryInfo.textContent = `Total number of books: ${myLibrary.length}`;
+}
+
+function displayLibraryBtn(){
+    if (libraryOptions.tableShown) {
+        displayBooksBtn.textContent = "Display library books";
+        booksTable.hidden = true;
+    } else {
+        displayBooksBtn.textContent = "Hide books table";
+        booksTable.hidden = false;
+        displayLibrary();
+    }
+    libraryOptions.tableShown = !libraryOptions.tableShown;
+}
+
 function displayLibrary(){
-    libraryTable = 
+    booksTable.innerHTML = getBookTableHeader() + getBookTableRows();
+}
+
+function getBookTableHeader(){
+    tableHeader = 
     `<colgroup>
         <col class="table-column-1">
         <col class="table-column-2">
@@ -47,7 +68,13 @@ function displayLibrary(){
     <th>Title</th>
     <th>Author</th>
     <th>Pages</th>
-    <th>Has been read</th>`
+    <th>Have read?</th>`
+
+    return tableHeader;
+}
+
+function getBookTableRows(){
+    let tableRows = '';
     for (let i = 0; i < myLibrary.length; i++){
         let currentBook = myLibrary[i];
         let bookRow = "<tr>\n"
@@ -60,18 +87,29 @@ function displayLibrary(){
             }
         }
         bookRow += '</tr>'
-        libraryTable += bookRow;
+        tableRows += bookRow;
     }
-    booksTable.innerHTML = libraryTable;
+    return tableRows;
 }
 
+const libraryInfo = document.querySelector('.library-info');
+const booksTable = document.querySelector('.books-table');
+const displayBooksBtn = document.querySelector('.display-books-btn');
+const formModal = document.querySelector('.form-modal');
 const form = document.querySelector('#book-form');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const pagesInput = document.querySelector('#pages');
 const readCheckbox = document.querySelector('#read');
-const submitButton = document.querySelector('.submit-btn');
-submitButton.addEventListener('click', (event) => {
+const newBookBtn = document.querySelector('.add-book');
+const modalAddBookBtn = document.querySelector('.modal-add-btn');
+const modalCancelBtn = document.querySelector('.modal-cancel-btn');
+
+// Page initialisation 
+updateLibraryInfo();
+displayBooksBtn.onclick = displayLibraryBtn;
+
+modalAddBookBtn.addEventListener('click', (event) => {
     if (form.checkValidity()) {
         event.preventDefault();
     }
@@ -79,5 +117,17 @@ submitButton.addEventListener('click', (event) => {
         titleInput.value,
         authorInput.value,
         pagesInput.value,
-        readCheckbox.checked)
+        readCheckbox.checked);
+    updateLibraryInfo();
+    if (libraryOptions.tableShown) displayLibrary();
+    form.reset();
+})
+
+newBookBtn.addEventListener('click', () => {
+    formModal.style.display = 'block';
+})
+
+modalCancelBtn.addEventListener('click', () => {
+    formModal.style.display = 'none';
+    form.reset();
 })
