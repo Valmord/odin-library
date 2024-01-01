0b30e324174ffd15d78486093a27c006be2abfc3
TC <nothing@nothing.com>
2024-01-02 00:12:40 +1300

    Initial commit with file structure done

---
+++ code.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, pages, author){
+    this.title = title;
+    this.pages = pages;
+    this.author = author;
+}
+
+function addBookToLibrary(){
+    //tbc
+}
