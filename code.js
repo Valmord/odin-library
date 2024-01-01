241bc6885b0d816e9f0e7922b08f1f77fad77be7
TC <nothing@nothing.com>
2024-01-02 00:51:08 +1300

    Book functionality added with sample books

--- code.js
+++ code.js
@@ -1,11 +1,38 @@
-const myLibrary = [];
+const myLibrary = [
+    {   title:"The Secret of Terror Castle", 
+        author:"Robert Arthur", 
+        pages: 108, 
+        haveRead: true
+    },
+    {   title:"The Mystery of the Stuttering Parrot", 
+        author:"Robert Arthur", 
+        pages: 111, 
+        haveRead: true
+    },
+    {   title:"The Mystery of the Whispering Mummy", 
+        author:"Robert Arthur", 
+        pages: 111, 
+        haveRead: true
+    },
+    {   title:"The Hunger Games", 
+        author:"Syzanne Collins", 
+        pages: 367, 
+        haveRead: false
+    },
+];
 
-function Book(title, pages, author){
+function Book(title, author, pages, haveRead){
     this.title = title;
     this.pages = pages;
     this.author = author;
+    this.haveRead = haveRead;
 }
 
-function addBookToLibrary(){
-    //tbc
+function addBookToLibrary(title, author, pages, haveRead){
+    const newBook = new Book(title, author, pages, haveRead);
+    myLibrary.push(newBook);
+}
+
+function displayLibrary(){
+    //
 }
